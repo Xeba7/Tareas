f88b8695277a0b6e9d077410df8b472ecd456f75
Xeba <xeba.cr@gmail.com>
2023-01-12 01:09:46 -0300

    change date, complete, delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./App.css";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -14,7 +13,7 @@ function TodoList() {
     if (newTodo === "") return;
     setTodos([
       ...todos,
-      { text: newTodo, completed: false, timestamp: Date.now() },
+      { text: newTodo, completed: false, created: Date.now() },
     ]);
     setNewTodo("");
   }
@@ -26,6 +25,7 @@ function TodoList() {
           return {
             text: todo.text,
             completed: !todo.completed,
+            created: todo.created,
           };
         }
         return todo;
@@ -37,14 +37,14 @@ function TodoList() {
     setTodos(todos.filter((todo, i) => i !== index));
   }
 
-  function handleToggleFavorite(index) {
+  function handleToggleComplete(index) {
     setTodos(
       todos.map((todo, i) => {
         if (i === index) {
           return {
             text: todo.text,
-            completed: todo.completed,
-            favorite: !todo.favorite,
+            completed: !todo.completed,
+            created: todo.created,
           };
         }
         return todo;
@@ -52,32 +52,40 @@ function TodoList() {
     );
   }
 
+  const getFormattedDate = (created) => {
+    return new Date(created).toLocaleString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
   return (
-    <div>
-      <h1>Todo List</h1>
+    <div className="container  w-1/2  text-center text-white py-6 mt-40">
+      <h1 className="text-6xl">Tareas</h1>
       <form onSubmit={handleAddTodo}>
         <input
+          className="p-1 px-3 rounded-2xl text-black"
           type="text"
           value={newTodo}
           onChange={handleNewTodoChange}
           placeholder="Add a new todo"
         />
-        <button type="submit" disabled={newTodo === ""}>
-          Add Todo
+        <button
+          className="border-4 border-white rounded-full p-2"
+          type="submit"
+          disabled={newTodo === ""}
+        >
+          Agregar
         </button>
       </form>
       <ul>
         {todos.map((todo, index) => {
-          const date = new Date(todo.timestamp).toLocaleString("en-US", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-          });
           return (
             <li key={index}>
-              <span>{date}</span>
+              <span>{getFormattedDate(todo.created)}</span>
               <span
                 style={{
                   textDecoration: todo.completed ? "line-through" : "none",
@@ -86,10 +94,10 @@ function TodoList() {
               >
                 {todo.text}
               </span>
-              <button onClick={() => handleToggleFavorite(index)}>
-                {todo.favorite ? "★" : "☆"}
+              <button onClick={() => handleToggleComplete(index)}>
+                {todo.completed ? "✔️" : "🕐"}
               </button>
-              <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+              <button onClick={() => handleDeleteTodo(index)}>🗑</button>
             </li>
           );
         })}
